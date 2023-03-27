fafebf3c52317bb2d4e9c99b3ed9eef7dbac4cf2
andy <andrea.tol.flores@gmail.com>
2023-03-27 16:45:51 -0600

    Se sube el primer ejercicio de Ciclos visto en clase.

---
+++ Ciclos/main.js
@@ -0,0 +1,19 @@
+
+/*-----------------Ejercicio1-----------------*/
+
+//Parte 1
+let nombresFila = ["Sofia", "David", "Juan",];
+
+console.log(nombresFila);
+
+// Parte 2
+nombresFila.push("Sara", "Agustin");
+console.log(nombresFila);
+nombresFila.shift();
+console.log(nombresFila);
+
+//Parte 3
+nombresFila.push("Elena");
+nombresFila.splice(1, 0, "Renata");
+console.log(nombresFila);
+
